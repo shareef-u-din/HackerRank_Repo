17c625fa9f6f3f1e1f30182798f4706e1a4ad501
Shareef U Din Bhat <shareef.b@techaspect.com>
2018-09-26 15:25:07 +0530

    Added Source Code to JavaScript insert node at a specific position in a Linked List

---
+++ Day_5/insertnodeatspecificpositioninlinkedlist.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+// Complete the insertNodeAtPosition function below.
+function insertNodeAtPosition(head, data, position) {
+    var newNode= new SinglyLinkedListNode(data);
+    if(position==0){
+        newNode.next=head;
+        return newNode;
+    }
+    var i=0;
+    var forward=head;
+    var previous=head;
+    while(forward != null){
+        if(i==position){
+            previous.next=newNode;
+            newNode.next=forward;
+            break;
+        }
+        previous=forward;
+        forward=forward.next;
+        i++;
+    }
+    
+ return head;
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const llistCount = parseInt(readLine(), 10);
+
+    let llist = new SinglyLinkedList();
+
+    for (let i = 0; i < llistCount; i++) {
+        const llistItem = parseInt(readLine(), 10);
+        llist.insertNode(llistItem);
+    }
+
+    const data = parseInt(readLine(), 10);
+
+    const position = parseInt(readLine(), 10);
+
+    let llist_head = insertNodeAtPosition(llist.head, data, position);
+
+    printSinglyLinkedList(llist_head, " ", ws)
+    ws.write("\n");
+
+    ws.end();
+}
