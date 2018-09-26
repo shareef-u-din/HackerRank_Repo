f4c1d3b69ef098b5bd5205dc1f2e0dfd0f575371
Shareef U Din Bhat <shareef.b@techaspect.com>
2018-09-26 15:45:34 +0530

    Added Source Code to JavaScript delete node in a Linked List

---
+++ Day_5/deletenodeinlinkedlist.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+// Complete the deleteNode function below.
+
+function deleteNode(head, position) {
+        let forward=head;
+        let previous=head;
+        if(position==0){
+            head=head.next;
+            forward=null;
+            return head;
+        }
+        let i=0;
+        while(forward != null){
+            if(i==position){
+               var temp=forward;
+                previous.next=forward.next;
+                forward=forward.next;
+                temp=null;
+                break;
+            }
+            i++;
+            previous=forward;
+            forward=forward.next;
+        }
+    return head;
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const llistCount = parseInt(readLine(), 10);
+
+    let llist = new SinglyLinkedList();
+
+    for (let i = 0; i < llistCount; i++) {
+        const llistItem = parseInt(readLine(), 10);
+        llist.insertNode(llistItem);
+    }
+
+    const position = parseInt(readLine(), 10);
+
+    let llist1 = deleteNode(llist.head, position);
+
+    printSinglyLinkedList(llist1, " ", ws)
+    ws.write("\n");
+
+    ws.end();
+}
