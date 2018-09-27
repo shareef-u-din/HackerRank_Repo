a2d341ea5df48d588c6a40d246179eb52b5a3ad8
Shareef U Din Bhat <shareef.b@techaspect.com>
2018-09-27 16:58:47 +0530

    Added Source Code to JavaScript array left rotation

---
+++ Day_5/arrayleftrotation.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+     var arr=new Array(n);
+        for(var i=0;i<n;i++){
+            arr[(i+n-d)%n]=a[i];
+        }
+    var combine="";
+        for(var i=0;i<n;i++){
+            combine+=arr[i]+" ";
+        }
+    console.log(combine);
+}
