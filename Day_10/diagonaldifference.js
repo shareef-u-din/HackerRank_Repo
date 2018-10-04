f6c3275d45206bd1b76a2b236da61d95a6f8b913
Shareef U Din Bhat <shareef.b@techaspect.com>
2018-10-04 12:02:52 +0530

    Added Solution in JavaScript Diagonal Difference Challenge

---
+++ Day_10/diagonaldifference.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the diagonalDifference function below.
+function diagonalDifference(arr) {
+    let principle=0;
+    let secondary=0;
+    let n=arr.length;
+    console.log(n);
+    for(var i=0;i<n;i++){
+        for(var j=0; j<n;j++) {
+            if(i == j)
+                principle += arr[i][j];
+            if(i+j == n-1)
+                secondary += arr[i][j];
+            
+        }
+    }
+    return Math.abs(secondary - principle);
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
