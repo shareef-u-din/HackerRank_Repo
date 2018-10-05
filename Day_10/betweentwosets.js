5977c8f892d7794fbf110a48aebc72345e8f4ffe
Shareef U Din Bhat <shareef.b@techaspect.com>
2018-10-05 10:32:28 +0530

    Added Solution in JavaScript Between Two Sets Challenge

---
+++ Day_10/betweentwosets.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the getTotalX function below.
+ */
+function getTotalX(a, b) {
+    let counter=0;
+    let aMax=Math.max(...a);
+    let bMax=Math.max(...b);
+    for(var i=aMax; i<=bMax;i++) {
+        if(a.every(v => i%v==0)){
+            
+         if(b.every(v=> v%i==0)) {
+             counter++;
+         } 
+        }
+    }
+    return counter;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nm = readLine().split(' ');
+
+    const n = parseInt(nm[0], 10);
+
+    const m = parseInt(nm[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    let total = getTotalX(a, b);
+
+    ws.write(total + "\n");
+
+    ws.end();
+}
