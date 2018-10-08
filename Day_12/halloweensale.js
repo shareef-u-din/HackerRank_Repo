8045ae041eb26f92ef14eaee0fe5fab5b3d55442
Shareef U Din Bhat <shareef.b@tadigital.com>
2018-10-08 18:44:07 +0530

    Added Source Code using JavaScript to Halloween Sale Challenge

---
+++ Day_12/halloweensale.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the howManyGames function below.
+function howManyGames(p, d, m, s) {
+    // Return the number of games you can buy
+    var sum = 0;
+    var count = 0;
+   
+    while(sum <= s) {
+        
+            sum += p;
+            count++;
+            p = p - d;
+            p = Math.max(p,m);
+           
+    }
+    return count-1;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const pdms = readLine().split(' ');
+
+    const p = parseInt(pdms[0], 10);
+
+    const d = parseInt(pdms[1], 10);
+
+    const m = parseInt(pdms[2], 10);
+
+    const s = parseInt(pdms[3], 10);
+
+    let answer = howManyGames(p, d, m, s);
+
+    ws.write(answer + "\n");
+
+    ws.end();
+}
