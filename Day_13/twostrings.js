aa27975c8252754f95d450c6b4cae5a52eb85ba9
Shareef U Din Bhat <shareef.b@tadigital.com>
2018-10-12 12:10:40 +0530

    Added  Source Code using JavaScript Two Strings Challenge

---
+++ Day_13/twostrings.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the twoStrings function below.
+function twoStrings(s1, s2) {
+    var status="NO";
+    var count =0;
+    var str="abcdefghijklmnopqrstuvwxyz";
+     var arr = str.split('');
+    for(let i=0;i<s1.length;i++) {
+        var val = s1.charAt(i);
+            if(s2.indexOf(val) > -1) {
+                status = "YES";
+                break;
+            } else {
+                status = "NO";
+                continue;
+            }
+    }
+    return status;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s1 = readLine();
+
+        const s2 = readLine();
+
+        let result = twoStrings(s1, s2);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
